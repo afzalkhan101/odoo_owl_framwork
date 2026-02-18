edaf08a80f52579954008e86ed38b0e0a04623f5
afzalkhan101 <afzalkhan01765@gmail.com>
2026-02-18 12:47:10 +0600

    init code

--- counter_app/static/src/js/counter.js
+++ counter_app/static/src/js/counter.js
@@ -2,36 +2,44 @@
 
 import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 export class Counter extends Component {
+    
     setup() {
-        // Initialize state with a reactive object
         this.state = useState({ 
             value: 0,
-            max:0,
-            min:0
-        
+            max: 10,  
+            min: 0 ,
         });
+        this.notificationService = useService("notification");
     }
 
     increment() {
         if (this.state.value < this.state.max){
              this.state.value++;
+        } else {
+            this.notificationService.add(`Maximum value (${this.state.max}) reached!`, {
+                type: "warning",
+            });
         }
-        
     }
 
     decrement() {
         if(this.state.value > this.state.min){
             this.state.value--;
+        } else {
+            this.notificationService.add(`Minimum value (${this.state.min}) reached!`, {
+                type: "warning",
+            });
         }
     }
 
     reset() {
         this.state.value = this.state.min;
+        this.notificationService.add("Counter reset!", { type: "info" });
     }
 }
-
 
 Counter.template = "counter_app.Counter"; 
 registry.category("actions").add("counter_app.counter_action", Counter);
